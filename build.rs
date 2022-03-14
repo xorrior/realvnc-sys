584e58927b5676d4540dd3cbc28d889c26c23e2d
xorrior <christopher.ross@zoom.us>
2022-03-13 22:14:44 -0400

    Added bindgen files

--- build.rs
+++ build.rs
@@ -1,32 +1,8 @@
-extern crate bindgen;
 
-use std::env;
-use std::fs::OpenOptions;
-use std::io::Write;
-use std::path::{Path, PathBuf};
 
 fn main() {
 
     println!("cargo:rerun-if-changed=wrapper.h");
     println!("cargo:rustc-link-search={}", "/usr/local/lib");
     println!("cargo:rustc-link-lib=vnc");
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let generated = bindings.to_string();
-
-    let out_path = out_path.join("bindings.rs");
-    let mut out_file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open(out_path)
-        .expect("Unable to open file");
-
-    out_file.write_all(generated.as_bytes())
-        .expect("Unable to write bindings file");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,49 @@
 #![allow(non_snake_case)]
 
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+extern crate libc;
+
+pub use init::*;
+pub use addon::*;
+pub use annotation::*;
+pub use cloud::*;
+pub use common::*;
+pub use databuffer::*;
+pub use datastore::*;
+pub use directtcp::*;
+pub use directudp::*;
+pub use displays::*;
+pub use errors::*;
+pub use eventloop::*;
+pub use eventloopfd::*;
+pub use eventloopwin::*;
+pub use keyboard::*;
+pub use logger::*;
+pub use messaging::*;
+pub use pixelformat::*;
+pub use rsakey::*;
+pub use version::*;
+pub use viewer::*;
+
+pub mod init;
+pub mod addon;
+pub mod annotation;
+pub mod cloud;
+pub mod common;
+pub mod databuffer;
+pub mod datastore;
+pub mod directtcp;
+pub mod directudp;
+pub mod displays;
+pub mod errors;
+pub mod eventloop;
+pub mod eventloopfd;
+pub mod eventloopwin;
+pub mod keyboard;
+pub mod logger;
+pub mod messaging;
+pub mod pixelformat;
+pub mod rsakey;
+pub mod server;
+pub mod version;
+pub mod viewer;
